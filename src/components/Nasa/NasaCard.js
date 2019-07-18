752b1633a284d35d670d563365cadb80d6c92be0
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-07-17 17:43:19 -0700

    added date selection. iframe integration for video.

--- src/components/Nasa/NasaCard.js
+++ src/components/Nasa/NasaCard.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from "react";
 
-export default function NasaCard({ title, date, url }) {
+export default function NasaCard({ title, date, url, type }) {
   return (
     <div className='cardImg'>
       <h4 className='nasaCardTitle'>{title}</h4>
-      <img src={url} style={{ maxWidth: "150px" }} />
+      {/* <img src={url} style={{ maxWidth: "150px" }} /> */}
+      {type === "image" ? (
+        <img src={url} style={{ maxWidth: "150px" }} /> 
+      ) : (
+        <iframe
+          title="video"
+          src={url}
+          width="560"
+          height="315"
+          allowFullScreen
+          align="middle"
+        ></iframe>
+      )
+      }
       <div className='nasaCardDate'>{date}</div>
     </div>
+    
   );
 }
--- src/components/Nasa/NasaGrid.js
+++ src/components/Nasa/NasaGrid.js
@@ -1,28 +1,42 @@
 import React, { useState, useEffect } from "react";
+import DatePicker from 'react-date-picker';
 import axios from 'axios';
 import NasaCard from './NasaCard.js';
 import BodyText from '../Display/BodyText.js';
 
 export default function NasaGrid() {
   const [nasaPic, setNasaPic] = useState([]);
+  const [date, setDate] = useState(new Date());
+  let dateQuery = '';
+  const year = date.getFullYear().toString(),
+    month = date.getMonth().toString(),
+    day = date.getDay().toString();
+  dateQuery = `${year}-${month}-${day}`;
+
+  const onChange = (date) => {
+    setDate(date);
+  }
 
   useEffect(() => {
     axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14')
+      .get(`https://api.nasa.gov/planetary/apod?api_key=sU0uSLBmVfJpkNHTa4hARZ80DBVSk1xHdUhFCADz&date=${dateQuery}`)
       .then(response => {
-        const nasaInfo = response.data
-        setNasaPic(nasaInfo)
-        console.log('nasaInfo', nasaInfo)
+        setNasaPic(response.data)
+        // console.log('nasaInfo', nasaInfo)
 
       });
-  }, []);
+  }, [date]);
 
   console.log('nasaPic:', nasaPic)
 
   return (
     <div className="nasa-grid">
-      <NasaCard title={nasaPic.title} date={nasaPic.date} url={nasaPic.url} />
+      <DatePicker onChange={onChange} value={date}/>
+      <NasaCard title={nasaPic.title} date={nasaPic.date} url={nasaPic.url} type={nasaPic.media_type} />
+      
       <BodyText text={nasaPic.explanation}/>
+      {/* <DateSelection /> */}
+      
     </div>
   );
 }
